1b4e262cf0e90aaa943f44949496c4ce4b29126f
owesleymauricio <wesleymauricio85@gmail.com>
2024-03-06 17:33:42 -0300

    tomara que funcione

---
+++ otica/dist/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"37lu09niGwBUW2JKTeLISDeOBP9+XgRB4y0ClIXOmdU=\"}"
--- otica/next.config.mjs
+++ otica/next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    distDir: 'dist', // Configuração para a pasta de saída
+  };
+  
 export default nextConfig;
+  
