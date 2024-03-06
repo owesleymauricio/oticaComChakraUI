f8c04e0e11264d21c114031f97e040dee49ae7ca
owesleymauricio <wesleymauricio85@gmail.com>
2024-03-06 18:22:50 -0300

    desisto hoje

--- otica/next.config.mjs
+++ otica/next.config.mjs
@@ -1,10 +1,8 @@
-/** @type {import('next').NextConfig}*/
-const nextConfig = {
-    // Outras configurações...
-    experimental: {
-      output: 'export',
-    },
-  };
-  
-  export default nextConfig;
-  
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+
+
+
+export default nextConfig;
+// next.config.js
